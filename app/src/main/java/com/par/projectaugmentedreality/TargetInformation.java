fe08b82b2fe133186a2e2e5ed85ba6803e521097
Maickel <maickelsmail@gmail.com>
2017-04-10 18:07:48 +0200

    Added dynamic string recognition

--- app/src/main/java/com/par/projectaugmentedreality/TargetInformation.java
+++ app/src/main/java/com/par/projectaugmentedreality/TargetInformation.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import com.par.projectaugmentedreality.R;
+
+import java.lang.reflect.Field;
 
 public class TargetInformation extends Activity {
     TextView TargetInfoText;
@@ -19,7 +22,18 @@ public class TargetInformation extends Activity {
         Intent intent = getIntent();
         String name = intent.getStringExtra("Dataset");
 
-        TargetInfoText.setText(name);
+        int id = getId(name, R.string.class);
 
+        TargetInfoText.setText(id);
+    }
+
+    public static int getId(String resourceName, Class<?> c) {
+        try {
+            Field idField = c.getDeclaredField(resourceName);
+            return idField.getInt(idField);
+        } catch (Exception e) {
+            throw new RuntimeException("No resource ID found for: "
+                    + resourceName + " / " + c, e);
+        }
     }
 }
